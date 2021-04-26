25be22104d864a4ca301da681443857df519cfe7
Daniel Gomez <danielsgg@gmail.com>
2021-04-26 15:05:12 -0500

    se agrega ciclo for para darle  funcionalidad al serch,se pone capitalize en la respuesta del input- css, se agrega funcion clearHtml para resetear imagenes, se hizo el evento Keycode para darle busqueda con tecla ENTER

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const navMenu = document.getElementById("navmenu")
 const favorito = document.getElementById("favorito");
 const misGifos = document.getElementById("misGifos");
 const circulo = document.getElementById("circulo");
-const buscador = document.getElementById("section-1")
+const buscador = document.getElementById("section-1");
+
+//variable para limitat busqueda de imagenes
+var limit= 12;
 
 
 menuHamburguesa.addEventListener("click", () => {
@@ -34,7 +37,7 @@ secciones(btnFavoritos, "click", favorito, circulo, buscador, misGifos);
 function search() {
     let palabra = document.getElementById("palabra").value;
     const apikey = "hvAnUXZU94XADuL3FIuxO3LjisOHTCRO";
-    let searchEndpoint = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + palabra + "&limit=25&offset=0&rating=g&lang=en";
+    let searchEndpoint = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + palabra + "&limit="+ limit +"&offset=0&rating=g&lang=en";
 
     fetch(searchEndpoint)
         .then(response => response.json())
@@ -42,16 +45,32 @@ function search() {
     }
 
 function crearImg(respuesta) {
-    var urlImg = respuesta.data[0].images.downsized.url;
-    var img = new Image();
-    img.src = urlImg;
-    document.getElementById("images").appendChild(img);
+    clearHtml();
+    for(let i= 0; i<limit; i++){
+        var urlImg = respuesta.data[i].images.downsized.url;
+        var img = new Image();
+        img.src = urlImg;
+        document.getElementById("images").appendChild(img);  
+    }
+}
+function clearHtml(){
+    document.getElementById("images").innerHTML = '';
 }
 
 let text = document.getElementById("palabra");
 text.addEventListener("keyup",(event) =>{
     let inputTex = event.path[0].value;
-    document.querySelector("#idshare").innerHTML = inputTex. toUpperCase();
+    document.querySelector("#idshare").innerHTML = inputTex;
+});
+
+text.addEventListener("keyup",(event) =>{
+    let inputTex = event.path[0].value;
+    document.querySelector("#idshare").innerHTML = inputTex;
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        search();       
+      }
 });
 
 // function setearPalabraEnSearhTxt() {
@@ -59,8 +78,8 @@ text.addEventListener("keyup",(event) =>{
 //     document.getElementById("idshare").innerHTML = palabra;
 // }
     
-    // for( i = 0; i <= 12; i++ ){
-    //     img
-    // }
+//     for( i = 0; i <= 12; i++ ){
+//         img
+//     }
 
 
